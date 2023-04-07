08f0001752eddf3c9630d285402808d1e6deb445
cherry304 <62474432+cherry304@users.noreply.github.com>
2023-04-07 16:44:09 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "checking the Github actions" );
+        System.out.println( "checking the Github Actions" );
         
             
         
